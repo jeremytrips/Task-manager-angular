3def90b0813b274c7f4e7f5623afc1ab1bf22f0d
jeremytrips <51384000+jeremytrips@users.noreply.github.com>
2021-04-20 16:05:36 +0200

    Create task list viewer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListViewerComponent } from './task-list-viewer/task-list-viewer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TaskListViewerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/tasklist.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+
+import { retry, catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+export class TaskList {
+  id: string;
+  Title: string;
+  DateCreated: Date;
+  DateModified: Date;
+  Tasks: Array<Task>;
+  Description: String;
+};
+
+export class Task {
+  id: string;
+  Description: string;
+  DateCreated: Date;
+  IsDone: boolean;
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasklistService {
+  // REST API
+  endpoint = 'http://127.0.0.1:8000';
+
+  constructor(private httpClient: HttpClient) { }
+    httpHeader = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+    })
+  }
+
+  getTaskLists(): Observable<TaskList>{
+    return this.httpClient.get<TaskList>(this.endpoint+"/api/journal")
+    .pipe(
+      retry(1),
+      catchError(this.processError)
+    );
+  }
+
+  createTaskList(taskList: TaskList){
+    return this.httpClient.post(
+      this.endpoint+"/api/journal/create",
+      taskList
+    ).pipe(
+      retry(1),
+      catchError(this.processError)
+    );
+  }
+
+  processError(err) {
+    console.error(err); 
+    let message = '';
+    if(err.error instanceof ErrorEvent) {
+     message = err.error.message;
+    } else {
+     message = `Error Code: ${err.status}\nMessage: ${err.message}`;
+    }
+    return throwError(message);
+ }
+}
