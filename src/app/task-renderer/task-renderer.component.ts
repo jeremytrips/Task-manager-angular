b491c38d5b458029bba67097ff9cee8e90edccab
jeremytrips <51384000+jeremytrips@users.noreply.github.com>
2021-04-29 09:03:23 +0200

    bug fix

--- src/app/task-renderer/task-renderer.component.ts
+++ src/app/task-renderer/task-renderer.component.ts
@@ -14,21 +14,9 @@ export class TaskRendererComponent implements OnInit {
 
   constructor(public tasklistService: TasklistService) {
   }
-  
-  // // todo fix the error on put method
-  // @HostListener("click") onClick(){
-  //   this.tasklistService.setResetTask(this.task.id)
-  //   .subscribe((res)=>{
-  //     console.log("ok");
-      
-  //   });
-  // }
 
   onDelete() {
-    // this.deleteEvent.emit(this.tasklist.id);
-    console.log("hello");
     this.deleteEvent.emit(this.task.id);
-    
   }
 
   ngOnInit(): void {
